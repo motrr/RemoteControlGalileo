9064ce5436b64327d978adc6a1e7088284065bc8
Chris Harding <chris.harding@acm.org>
2012-07-02 16:48:33 -0700

    Removed JPEG encoding and fragmentation

---
+++ GalileoHD/VideoDecoder.m
@@ -0,0 +1,13 @@
+//
+//  VideoDecoder.m
+//  GalileoHD
+//
+//  Created by Chris Harding on 02/07/2012.
+//  Copyright (c) 2012 Swift Navigation. All rights reserved.
+//
+
+#import "VideoDecoder.h"
+
+@implementation VideoDecoder
+
+@end
